bd6a3bd137870c6b4a56c6a9a06d489848e5f9ba
luis <70785065+luisdob@users.noreply.github.com>
2022-11-18 14:45:58 -0400

    Add files via upload

---
+++ ASS_Final_parametros.m
@@ -0,0 +1,14 @@
+%% Constantes sistema mecánico
+M2 = 250;
+M1 = 40;
+K2 = 28000;
+K1 = 125000;
+B2 = 2000;
+B1 = 100;
+%% Constantes sistema eléctrico
+eta = 0.92;
+J = 0.15e-6;
+k = 0.456;
+io = 0;
+r = 0.4;
+L = 20e-3;
